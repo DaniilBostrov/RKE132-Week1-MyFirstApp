29933bf64905b03427a3a15bf289d6f74fc252a3
DaniilBostrov <daniil.bostrov@tktk.ee>
2023-01-29 22:11:09 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//This is a comment, to my code
+
+//rakendus kysib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//strinng - sõne
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");
